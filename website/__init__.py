49abb98dc849ef4a2ab391e33ee674c0603e57d7
Gilmar Neves <30027072+nevesgil@users.noreply.github.com>
2022-02-05 22:20:47 -0300

    Initial simple routes for testing (#1)

--- website/__init__.py
+++ website/__init__.py
@@ -5,5 +5,10 @@ def create_app():
     app = Flask(__name__)
     # try linking the secret on the key vault
     app.config['SECRET_KEY'] = 'senhakaren'
-
+    # those need to be here
+    from .views import views
+    from .auth import auth
+    # those need to be registered
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
     return app
