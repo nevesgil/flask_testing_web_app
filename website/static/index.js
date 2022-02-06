6d746002bb32a17f34865063cbf91d20e909cb60
nevesgil <gilmarneves@outlook.com>
2022-02-06 16:21:21 -0300

    Index JS function added

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,8 @@
-// Change heading:
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
