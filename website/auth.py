670920e8763041ade6a6b708f0a0da9d411e048a
nevesgil <gilmarneves@outlook.com>
2022-02-06 20:07:08 -0300

    Base templates

--- website/auth.py
+++ website/auth.py
@@ -4,11 +4,11 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", boolean=True)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("home.html")
 
 @auth.route('/sign-up')
 def sign_up():
