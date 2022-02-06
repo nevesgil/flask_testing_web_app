03bbc0c3f4b888183d738f49a6450f8665dab224
Gilmar Neves <30027072+nevesgil@users.noreply.github.com>
2022-02-06 20:09:01 -0300

    HTML Templates (#2)
    * Base html template
    
    * Update base.html
    
    * Update base html
    
    * Update base.html
    
    * Home and base templates updated
    
    * Cache script
    
    * Replaced the base.html file with the original one.
    
    * More templates
    
    * Index JS function added
    
    * Removing title and user code
    
    * Base templates

--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 # anything not related to authentication may go in this file
 # logins goes on the auth file
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 # whatever goes on home will run the bellow function
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
