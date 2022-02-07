dbcf96e9949a9efa5561d653f66ef0faa6d9f68c
Gilmar Neves <30027072+nevesgil@users.noreply.github.com>
2022-02-06 22:47:31 -0300

    Setup for SQLAlchemy (#4)

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     # try linking the secret on the key vault
-    app.config['SECRET_KEY'] = 'senhakaren'
+    app.config["SECRET_KEY"] = "senhakaren"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
     # those need to be here
     from .views import views
     from .auth import auth
+
     # those need to be registered
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     return app
