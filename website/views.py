ec5e6c8fb92eb1ae9db3146daf93912e08a8cf22
Gilmar Neves <30027072+nevesgil@users.noreply.github.com>
2022-02-07 21:56:51 -0300

    Feature login (#5)

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 # anything not related to authentication may go in this file
 # logins goes on the auth file
-
+from flask_login import login_required, current_user
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 # whatever goes on home will run the bellow function
-@views.route('/')
+@views.route("/")
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
