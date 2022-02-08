cf3ae71a11cec16dd0a4eb5bad83c24adf264f0f
nevesgil <gilmarneves@outlook.com>
2022-02-07 22:36:21 -0300

    Notes HTML

--- website/views.py
+++ website/views.py
@@ -1,12 +1,40 @@
 # anything not related to authentication may go in this file
 # logins goes on the auth file
+from unicodedata import category
 from flask_login import login_required, current_user
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from sympy import im
+from .models import Note
+from . import db
+import json
 
 views = Blueprint("views", __name__)
 
 # whatever goes on home will run the bellow function
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
+    if request.method == "POST":
+        note = request.form.get("note")
+
+        if len(note) < 1:
+            flash("Note is too short!", category="error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category="success")
     return render_template("home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note["noteId"]
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
